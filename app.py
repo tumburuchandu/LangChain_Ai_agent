cf53e0cf832377a4dce36ac14afdad212659540c
tumburuchandu <tumburuchandu@gmail.com>
2026-08-10 14:41:03 +0530

    Add project documentation

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 from langchain.tools import tool
 from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain import hub
+from langchain_core.prompts import PromptTemplate
 from langchain.agents import create_react_agent, AgentExecutor
 
 
@@ -266,9 +266,25 @@ tools = [
 # Agent Prompt
 # ============================================================
 
-prompt = hub.pull(
-    "hwchase17/react"
-)
+prompt = PromptTemplate.from_template("""
+Answer the following questions as best you can. You have access to the following tools:
+
+{tools}
+
+Use the following format:
+
+Question: the input question you must answer
+Thought: you should always think about what to do
+Action: the action to take, should be one of [{tool_names}]
+Action Input: the input to the action
+Observation: the result of the action
+... (this Thought/Action/Action Input/Observation can repeat)
+Thought: I now know the final answer
+Final Answer: the final answer to the original input question
+
+Question: {input}
+Thought: {agent_scratchpad}
+""")
 
 
 # ============================================================
